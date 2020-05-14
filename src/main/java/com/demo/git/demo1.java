b0a8d7301adc62debe5bf3605d921e5b41440e6e
root <qhza0403@163.com>
2020-05-14 12:20:49 +0800

    TestGit2 commit

--- src/main/java/com/demo/git/demo1.java
+++ src/main/java/com/demo/git/demo1.java
@@ -2,6 +2,7 @@ package com.demo.git;
 
 public class demo1 {
 	public static void main(String[] args) {
-		System.out.println("my demo1");
+		System.out.println("my TestGit2 demo1");
+		System.out.println("this is master");		
 	}
 }
