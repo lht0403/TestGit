792f501844c6a498d17ea7ba57be390d7289c3ea
root <qhza0403@163.com>
2020-05-14 15:26:34 +0800

    my TestGit hot_fix

--- src/main/java/com/demo/git/demo1.java
+++ src/main/java/com/demo/git/demo1.java
@@ -3,7 +3,6 @@ package com.demo.git;
 public class demo1 {
 	public static void main(String[] args) {
 		System.out.println("my demo1");
-		System.out.println("this is hot_fix");
-		
+		System.out.println("this is hot_fix");		
 	}
 }
